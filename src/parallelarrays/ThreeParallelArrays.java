d8efdad7da75d7490d901cfbeb4a99fc175ec887
Jack Bowman <jbbowman@students.unwsp.edu>
2022-10-13 20:46:05 -0500

    Created ThreeParallelArrays.java; user can enter a state name to get abbreviation and capitol, then it asks the user for zip code and appends this to the capitol name

--- src/parallelarrays/ThreeParallelArrays.java
+++ src/parallelarrays/ThreeParallelArrays.java
@@ -1,10 +1,37 @@
 package parallelarrays;
 
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 public class ThreeParallelArrays {
 
 	public static void main(String[] args) {
 		String states[] = {"Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"};
 		String statesAbbreviation[] = {"AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"};
-		String capitals[] = {"Montgomery","Juneau","Phoenix","Little Rock","Sacramento","Denver","Hartford","Dover","Tallahassee","Atlanta","Honolulu","Boise","Springfield","Indianapolis","Des Moines","Topeka","Frankfort","Baton Rouge","Augusta","Annapolis","Boston","Lansing","St. Paul","Jackson","Jefferson City","Helena","Lincoln","Carson City","Concord","Trenton","Santa Fe","Albany","Raleigh","Bismarck","Columbus","Oklahoma City","Salem","Harrisburg","Providence","Columbia","Pierre","Nashville","Austin","Salt Lake City","Montpelier","Richmond","Olympia","Charleston","Madison","Cheyenne"};
+		String capitols[] = {"Montgomery", "Juneau", "Phoenix", "Little Rock", "Sacramento", "Denver", "Hartford", "Dover", "Tallahassee", "Atlanta", "Honolulu", "Boise", "Springfield", "Indianapolis", "Des Moines", "Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis", "Boston", "Lansing", "St. Paul", "Jackson", "Jefferson City", "Helena", "Lincoln", "Carson City", "Concord", "Trenton", "Santa Fe", "Albany", "Raleigh", "Bismarck", "Columbus", "Oklahoma City", "Salem", "Harrisburg", "Providence", "Columbia", "Pierre", "Nashville", "Austin", "Salt Lake City", "Montpelier", "Richmond", "Olympia", "Charleston", "Madison", "Cheyenne"};
+
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			System.out.print("Enter a state name: ");
+			String stateInput = scanner.nextLine();
+			boolean inArray = false;
+
+			for (int i = 0; i < states.length; i++) {
+				if (stateInput.equals(states[i])) {
+					System.out.print("What is the zip code of the city: ");
+					String capZip = String.format("%s, %s",capitols[i], scanner.nextLine());
+					Array.set(capitols, i, capZip);
+
+					System.out.printf("%s's abbreviation is %s and its capitol is %s\n", states[i], statesAbbreviation[i], capitols[i]);
+					inArray = true;
+
+				}
+			}
+			if (!inArray) {
+				System.out.println("State not recognized\nEnding Program");
+				break;
+			}
+		}
 	}
 }
